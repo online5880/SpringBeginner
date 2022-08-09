566600a15185523010ccb42eb9a1649b86a00507
online5880 <gjtjqkr1@naver.com>
2022-08-09 23:28:52 +0900

    View 환경설정

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,7 +3,6 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
 public class HelloController {
 
